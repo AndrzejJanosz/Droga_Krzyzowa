b29adb3f540bf667b0da41ed5e1d327a2140bb1c
AndrzejJanosz <ajanosz@student.agh.edu.pl>
2025-03-13 23:43:08 +0100

    dodanie analityki

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import HomeButton from './components/HomeButton'
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   
@@ -15,6 +16,7 @@ function App() {
       <Outlet />
     </main>
     <Navbar />
+    <Analytics />
      
     </>
   )
