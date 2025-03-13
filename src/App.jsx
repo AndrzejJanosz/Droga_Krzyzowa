7860d436db7360abc18f58fe0225243ed75c1bf3
AndrzejJanosz <ajanosz@student.agh.edu.pl>
2025-03-13 23:58:06 +0100

    dodanie analityki  i speed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import HomeButton from './components/HomeButton'
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   
